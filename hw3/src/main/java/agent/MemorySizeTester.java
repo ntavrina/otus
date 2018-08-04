8c29ca9093376eb8d5d38bcb304611128163a760
Natalya Tavrina <ntavrina@gmail.com>
2018-08-04 22:45:46 +0300

    Домашнее задание #2: измерение памяти

--- hw3/src/main/java/agent/MemorySizeTester.java
+++ hw3/src/main/java/agent/MemorySizeTester.java
@@ -1,8 +1,10 @@
 package agent;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 public class MemorySizeTester {
+
     public static void main(String[] args) {
 
         printSize(new Object());
@@ -25,19 +27,32 @@ public class MemorySizeTester {
         int cycle = 3;
         int max_size = 1000 * 1000 * 5;
         ArrayList<String> al2 = new ArrayList<>();
+        Field[] F = ArrayList.class.getDeclaredFields();
         for(int i=0; i<cycle; ++i) {
             for(int j=0; j<max_size; ++j) {
                 al2.add(new String());
             }
-            System.out.println("Size of array of "+al2.size()+" elements:");
-            printSize(al2);
+            long size = 0;
+            for(Field f : F) {
+                f.setAccessible(true);
+                try {
+                    size += getSize(f.get(al2));
+                } catch (Exception e) {}
+            }
+            System.out.println("Size of arraylist of "+al2.size()+" elements: " + size + " bytes");
         }
 
     }
 
-    private static void printSize(Object o) throws NullPointerException {
+    private static void printSize(Object o) throws IllegalArgumentException {
         if(o == null)
-            throw new NullPointerException();
+            throw new IllegalArgumentException();
         System.out.println(o.getClass().getSimpleName()+": " + Agent.getSize(o) + " bytes");
+    }
+
+    private static long getSize(Object o) {
+        if(o == null)
+            return 0;
+        return Agent.getSize(o);
     }
 }
